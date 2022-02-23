6106154b542d89944e7fc5438671c063cac39397
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-02-23 14:47:39 +0100

    add-enum-bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
 
+  enum status: { pending: 0, approved: 1, declined: 2 }
+
   def self.my_bookings(user)
     where(user: user)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_070344) do
+ActiveRecord::Schema.define(version: 2022_02_23_124634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_070344) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "user_id", null: false
     t.bigint "dog_id", null: false
     t.datetime "created_at", precision: 6, null: false
