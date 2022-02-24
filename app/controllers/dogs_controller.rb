32f5970b03df5054bb945064998d6dfeb90ce571
Sander Koreman <sanderkoreman@gmail.com>
2022-02-24 17:58:42 +0100

    fixed icon

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,7 +8,7 @@ class DogsController < ApplicationController
         lat: flat.latitude,
         lng: flat.longitude,
         info_window: render_to_string(partial: "info_window", locals: { flat: flat }),
-        image_url: helpers.asset_url("dogface5")
+        image_url: helpers.asset_url("dogface5.svg")
       }
     end
   end
