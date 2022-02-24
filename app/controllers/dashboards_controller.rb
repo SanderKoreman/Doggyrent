23609b296910b9cbd6f4afe3914a329eb8e47c8d
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-02-24 15:13:00 +0100

    created tab for requested bookings

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,10 +3,10 @@ class DashboardsController < ApplicationController
     @my_bookings = Booking.my_bookings(current_user)
     # byebug
     @received_bookings = Booking.received_bookings(current_user)
-
+    @archived_bookings = Booking.archived_bookings(current_user)
+    
     # @my_dogs = Dog.where(user: current_user)
     @my_dogs = current_user.dogs
     @user = current_user
   end
-
 end
